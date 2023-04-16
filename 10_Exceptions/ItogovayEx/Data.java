2f830dda01a64fd258a67bef59982bbd32dd821e
ZakirovaVera <zakirovavk@gmail.com>
2023-04-16 23:48:13 +0500

    итоговая наброски классов

---
+++ 10_Exceptions/ItogovayEx/Data.java
@@ -0,0 +1,60 @@
+import java.util.Date;
+
+public class Data {
+    private String family;
+    private String name;
+    private String otchestvo;
+    private Date dateOfBirth;
+    private Integer phone;
+    private String sex;
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOtchestvo() {
+        return otchestvo;
+    }
+
+    public void setOtchestvo(String otchestvo) {
+        this.otchestvo = otchestvo;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+
+}
---
+++ 10_Exceptions/ItogovayEx/Parser.java
@@ -0,0 +1,9 @@
+public class Parser {
+
+    public Data name(String consoleData) {
+        Data data = new Data();
+
+        return data;
+    }
+
+}
