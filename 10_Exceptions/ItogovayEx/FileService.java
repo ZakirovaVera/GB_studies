2e9f422b62180d7a2bd8bb0435ad030b87cb188e
ZakirovaVera <zakirovavk@gmail.com>
2023-05-13 17:28:10 +0500

    Parse

--- 10_Exceptions/ItogovayEx/FileService.java
+++ 10_Exceptions/ItogovayEx/FileService.java
@@ -1,3 +1,42 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileService {
-    
+    // Папка для хранения.
+    final String folderToSave = "Records";
+    // Текущее расположение приложения.
+    final String currentDir = System.getProperty("user.dir");
+
+    public void SaveToFile(ItemRecord data) throws IOException {
+        var path = GetFullPath();
+
+        var directory = new File(path);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        var fileByFam = new File(path + "\\" + data.getFamily() + ".txt");
+        var recordDelemiter = "\n";
+        if (!fileByFam.exists()) {
+            fileByFam.createNewFile();
+            recordDelemiter = "";
+        }
+
+        FileWriter writer = new FileWriter(fileByFam, true);
+        var text = recordDelemiter +
+                "<" + data.getFamily() + ">" +
+                "<" + data.getName() + ">" +
+                "<" + data.getOtchestvo() + ">" +
+                "<" + data.getDateOfBirth() + ">" +
+                "<" + data.getPhone() + ">" +
+                "<" + data.getSex() + ">";
+        writer.write(text);
+        writer.flush();
+        writer.close();
+    }
+
+    private String GetFullPath() {
+        return currentDir + "\\" + folderToSave;
+    }
 }
--- 10_Exceptions/ItogovayEx/Parser.java
+++ 10_Exceptions/ItogovayEx/Parser.java
@@ -1,39 +1,75 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 public class Parser {
 
-    public Data parceToData(String consoleData) {
+    public ItemRecord parceToData(String consoleData) throws NumberOfDataFieldsException, FormatDataException {
         String[] arrayData = consoleData.split(" ");
-        if (arrayData.length != Data.CountFields) {
+        if (arrayData.length != ItemRecord.CountFields) {
             throw new NumberOfDataFieldsException("Введено неверное количество полей");
         }
 
-        Data data = new Data();
+        ItemRecord data = new ItemRecord();
         data.setFamily(parseToFIO(arrayData[0]));
         data.setName(parseToFIO(arrayData[1]));
         data.setOtchestvo(parseToFIO(arrayData[2]));
         data.setDateOfBirth(parseToDOB(arrayData[3]));
-        data.setPhone(parseToPh(arrayData[4]));
+        data.setPhone(parseToPhone(arrayData[4]));
         data.setSex(parseToSex(arrayData[5]));
+
         return data;
     }
 
-    private String parseToFIO(String data) {
+    private String parseToFIO(String data) throws FormatDataException {
         if (data == null || data.isEmpty()) {
-            throw new FormatDataException("Некорректные данные - ФИО");
+            throw new FormatDataException("Некорректные данные - ФИО.");
         }
 
         String result = stringToCamelCase(data);
         return result;
     }
 
-    private Date parseToDOB(String data) {
-        //TODO распарсить строку в свойство
+    // Распарсить в дату рождения.
+    private String parseToDOB(String data) throws FormatDataException {
+        try {
+            String[] parts = data.split(".");
+
+            if (parts.length < 3) {
+                throw new FormatDataException("Необходимо 3 части через точку.");
+            }
+
+            new GregorianCalendar(
+                    Integer.parseInt(parts[2]),
+                    Integer.parseInt(parts[1]),
+                    Integer.parseInt(parts[0]));
+
+            return data;
+        } catch (FormatDataException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new FormatDataException("Некорректные данные - Дата рождения.");
+        }
+    }
+
+    private Integer parseToPhone(String data) {
+        return Integer.parseInt(data);
+    }
+
+    private String parseToSex(String data) throws FormatDataException {
+        List<String> allowSymbol = new ArrayList<String>(Arrays.asList(new String[] { "f", "m" }));
+        if (!allowSymbol.contains(data)) {
+            throw new FormatDataException("Некорректные данные - пол.");
+        }
+
+        return data;
     }
 
     private String stringToCamelCase(String data) {
-        String part1 = data[0];
+        String part1 = data.substring(0, 1);
         String part2 = data.substring(1, data.length() - 1);
         String result = part1.toUpperCase() + part2.toLowerCase();
         return result;
     }
-
 }
