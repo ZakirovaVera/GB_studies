72d12d2cf8a6a1ccb9cf4386e7a6b26e8461ea9a
ZakirovaVera <zakirovavk@gmail.com>
2023-05-14 11:36:34 +0500

    FileCreationResult новый класс в FileService

--- 10_Exceptions/ItogovayEx/FileService.java
+++ 10_Exceptions/ItogovayEx/FileService.java
@@ -9,22 +9,15 @@ public class FileService {
     final String currentDir = System.getProperty("user.dir");
 
     public void SaveToFile(ItemRecord data) throws IOException {
+        // Путь папки
         var path = GetFullPath();
+        // Создать папку
+        CreateDirectory(path);
+        // Создать файл по фамилии
+        FileCreationResult fileByFam = CreateFile(path, data.getFamily());
 
-        var directory = new File(path);
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
-
-        var fileByFam = new File(path + "\\" + data.getFamily() + ".txt");
-        var recordDelemiter = "\n";
-        if (!fileByFam.exists()) {
-            fileByFam.createNewFile();
-            recordDelemiter = "";
-        }
-
-        FileWriter writer = new FileWriter(fileByFam, true);
-        var text = recordDelemiter +
+        FileWriter writer = new FileWriter(fileByFam.getFile(), true);
+        var text = fileByFam.getRecordDelemiter() +
                 "<" + data.getFamily() + ">" +
                 "<" + data.getName() + ">" +
                 "<" + data.getOtchestvo() + ">" +
@@ -36,7 +29,51 @@ public class FileService {
         writer.close();
     }
 
+    private FileCreationResult CreateFile(String path, String family) throws IOException {
+        FileCreationResult result = new FileCreationResult();
+
+        var fileByFam = new File(path + "\\" + family + ".txt");
+        result.setFile(fileByFam);
+
+        var recordDelemiter = "\n";
+        if (!fileByFam.exists()) {
+            fileByFam.createNewFile();
+            recordDelemiter = "";
+        }
+        result.setRecordDelemiter(recordDelemiter);
+
+        return result;
+    }
+
+    private void CreateDirectory(String path) {
+        var directory = new File(path);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+    }
+
     private String GetFullPath() {
         return currentDir + "\\" + folderToSave;
+    }
+}
+
+class FileCreationResult {
+    private File file;
+    private String recordDelemiter;
+
+    public void setFile(File value) {
+        file = value;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setRecordDelemiter(String value) {
+        recordDelemiter = value;
+    }
+
+    public String getRecordDelemiter() {
+        return recordDelemiter;
     }
 }
